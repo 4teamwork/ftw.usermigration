d8438567cd2e40177b9aba1baa87489cbe811ebf
Lukas Graf <lukas.graf@4teamwork.ch>
2015-04-13 16:05:29 +0200

    Make logging to file optional (default False).

--- ftw/usermigration/browser/migration.py
+++ ftw/usermigration/browser/migration.py
@@ -121,6 +121,14 @@ class IUserMigrationFormSchema(interface.Interface):
         default=False,
     )
 
+    log_to_file = schema.Bool(
+        title=_(u'label_log_to_file', default=u'Log migration report to file'),
+        default=False,
+        description=_(u'help_log_to_file',
+                      default=u'Whether a detailed migration report should be'
+                      ' written to a logfile on the filesystem.'),
+    )
+
     dry_run = schema.Bool(
         title=_(u'label_dry_run', default=u'Dry Run'),
         default=False,
@@ -146,6 +154,7 @@ class UserMigrationForm(form.Form):
         self.results_users = {}
         self.results_properties = {}
         self.results_post_migration = {}
+        self.log_to_file = False
 
     def _get_manual_mapping(self, formdata):
         manual_mapping = formdata['manual_mapping']
@@ -182,21 +191,26 @@ class UserMigrationForm(form.Form):
         for h in logger.handlers:
             logger.removeHandler(h)
 
-        timestamp = datetime.today().strftime('%Y%m%d%H%M')
-        log_fn = 'usermigration-{0}.log'.format(timestamp)
-        log_dir = get_var_log()
-        mkdir_p(log_dir)
-        log_path = os.path.join(log_dir, log_fn)
+        log_path = None
+        if self.log_to_file:
+            timestamp = datetime.today().strftime('%Y%m%d%H%M')
+            log_fn = 'usermigration-{0}.log'.format(timestamp)
+            log_dir = get_var_log()
+            mkdir_p(log_dir)
+            log_path = os.path.join(log_dir, log_fn)
 
-        handler = FileHandler(log_path)
-        handler.setFormatter(logging.Formatter(
-            "%(asctime)-15s %(message)s", "%Y-%m-%d %H:%M"))
-        handler.setLevel(logging.DEBUG)
-        logger.addHandler(handler)
+            handler = FileHandler(log_path)
+            handler.setFormatter(logging.Formatter(
+                "%(asctime)-15s %(message)s", "%Y-%m-%d %H:%M"))
+            handler.setLevel(logging.DEBUG)
+            logger.addHandler(handler)
 
         return logger, log_path
 
     def _log_migration_details(self, logger, name, results):
+        if not self.log_to_file:
+            return
+
         for mode in results.keys():
             for obj, old, new in results[mode]:
                 logger.debug("Migrated [{0}] {1}: {2} -> {3}".format(
@@ -211,6 +225,7 @@ class UserMigrationForm(form.Form):
             self.status = self.formErrorsMessage
             return
 
+        self.log_to_file = data['log_to_file']
         logger, logfile_path = self._setup_logger()
         logger.info('Starting principal migration')
 
@@ -290,7 +305,9 @@ class UserMigrationForm(form.Form):
                 self._log_migration_details(logger, section, hook_results[section])
 
         logger.info("Migration finished")
-        logger.info("Migration logfile written to {0}".format(logfile_path))
+        if self.log_to_file:
+            logger.info("Migration logfile written to {0}".format(
+                logfile_path))
 
         self.result_template = ViewPageTemplateFile('migration.pt')
 
